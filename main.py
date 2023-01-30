b0d9f648891d9ce3c3f0882c58ed4bd3e3c1eb72
User <user@gmail.com>
2023-01-30 21:07:25 +0300

    dasdas

---
+++ main.py
@@ -0,0 +1,85 @@
+import sys
+from io import BytesIO
+
+import os
+
+import pygame
+
+import requests
+from PIL import Image
+
+toponym_to_find = " ".join(sys.argv[1:])
+
+geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+geocoder_params = {
+    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+    "geocode": toponym_to_find,
+    "format": "json"}
+
+response = requests.get(geocoder_api_server, params=geocoder_params)
+
+if not response:
+    # обработка ошибочной ситуации
+    pass
+
+# Преобразуем ответ в json-объект
+json_response = response.json()
+# Получаем первый топоним из ответа геокодера.
+toponym = json_response["response"]["GeoObjectCollection"][
+    "featureMember"][0]["GeoObject"]
+# Координаты центра топонима:
+toponym_coodrinates = toponym["Point"]["pos"]
+# Долгота и широта:
+toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+
+delta = "0.005"
+
+# Собираем параметры для запроса к StaticMapsAPI:
+map_params = {
+    "ll": ",".join([toponym_longitude, toponym_lattitude]),
+    "spn": ",".join([delta, delta]),
+    "l": "map",
+    "pt": ",".join([toponym_longitude, toponym_lattitude, "pmbll"])
+}
+
+map_api_server = "http://static-maps.yandex.ru/1.x/"
+# ... и выполняем запрос
+response = requests.get(map_api_server, params=map_params)
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+all_sprites = pygame.sprite.Group()
+pygame.init()
+size = width, height = 800, 800
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('Большая задача по Maps API. Часть №1')
+image = Image.open(BytesIO(response.content))
+image.save("data/map.png")
+sprite = pygame.sprite.Sprite()
+sprite.image = load_image("map.png")
+sprite.rect = sprite.image.get_rect()
+all_sprites.add(sprite)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            os.remove('data/map.png')
+            running = False
+    all_sprites.draw(screen)
+    pygame.display.flip()
